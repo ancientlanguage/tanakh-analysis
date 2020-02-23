3f9f59a32efbae63960d1eb0513025a4982b9c38
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2020-02-23 06:52:38 -0800

    Add stub test.

--- haskell/test/Main.hs
+++ haskell/test/Main.hs
@@ -1,2 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+import qualified Control.Monad
+import qualified Hedgehog
+import qualified Hedgehog.Gen
+import qualified Hedgehog.Range
+import qualified System.Exit
+
 main :: IO ()
-main = putStrLn "Hello test"
+main = do
+  isSuccess <-
+    Hedgehog.checkParallel $
+      Hedgehog.Group
+        "Tanakh"
+        [ ("stubProperty", stubProperty)
+        ]
+  Control.Monad.unless isSuccess System.Exit.exitFailure
+
+stubProperty :: Hedgehog.Property
+stubProperty = Hedgehog.property $ do
+  number <- Hedgehog.forAll $ Hedgehog.Gen.integral @_ @Int $ Hedgehog.Range.linear 1 100
+  Hedgehog.assert (number > 0)
