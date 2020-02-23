43b717e2e8be3945c7ec8b9d8a7ba2bcd207cc15
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2020-02-23 07:42:46 -0800

    WIP: add property of basic sum trees.

--- haskell/src/Tanakh.hs
+++ haskell/src/Tanakh.hs
@@ -1,4 +1,96 @@
 module Tanakh where
 
+import GHC.Natural (Natural)
+
+data Nat
+  = Zero
+  | PlusOne Nat
+  deriving Eq
+
+natToNatural :: Nat -> Natural
+natToNatural Zero = 0
+natToNatural (PlusOne n) = 1 + natToNatural n
+
+{-
+Zero
+PlusOne NatUnit -- 1
+PlusOne (PlusOne NatUnit) -- 2
+PlusOne (PlusOne (PlusOne NatUnit)) -- 3
+
+Zero
+
+   PlusOne
+     Zero
+
+ PlusOne
+   PlusOne
+     Zero
+-}
+
+data Sum
+  = Unit
+  | Sum [Sum]
+  deriving (Eq, Show)
+
+{-
+Unit -- 1
+
+Sum []  -- 0
+Sum [Unit] -- 1
+Sum [Unit, Unit] -- 2
+Sum [Unit, Unit, Unit] --3
+
+Sum [Sum []] -- 0
+Sum [Sum [Unit]] -- 1
+Sum [Sum [Unit, Unit]] -- 2
+
+      Sum
+       []
+
+      Sum
+      [Unit]
+
+      Sum
+    [ Sum ]
+      []
+
+      Sum
+  [   Sum   ]
+     [Unit]
+
+      Sum
+ [    Sum      ]
+   [Unit,Unit]
+
+
+Sum [Sum [Unit, Unit], Sum []] -- 2
+
+      Sum
+ [    Sum      , Sum ]
+   [Unit,Unit]    []
+
+-}
+
+sumToNat :: Sum -> Natural
+sumToNat Unit = 1
+sumToNat (Sum nestedSums) = sum (fmap sumToNat nestedSums)
+
+natToNormalizedSum :: Natural -> Sum
+natToNormalizedSum n | n == 0 = Sum []
+natToNormalizedSum n | n == 1 = Unit
+natToNormalizedSum n = Sum (take (fromIntegral n) $ repeat Unit)
+
+flattenSum :: Sum -> [Sum]
+flattenSum Unit = [Unit]
+flattenSum (Sum []) = []
+flattenSum (Sum nestedSums@(_ : _)) = concatMap flattenSum nestedSums
+
+normalizeSum :: Sum -> Sum
+normalizeSum input =
+  case flattenSum input of
+    [] -> Sum []
+    [Unit] -> Unit
+    many@(_ : _) -> Sum many
+
 main :: IO ()
 main = putStrLn "hello!"
--- haskell/test/Main.hs
+++ haskell/test/Main.hs
@@ -2,9 +2,11 @@
 {-# LANGUAGE TypeApplications #-}
 
 import qualified Control.Monad
+import Hedgehog ((===))
 import qualified Hedgehog
 import qualified Hedgehog.Gen
 import qualified Hedgehog.Range
+import qualified Tanakh
 import qualified System.Exit
 
 main :: IO ()
@@ -13,11 +15,25 @@ main = do
     Hedgehog.checkParallel $
       Hedgehog.Group
         "Tanakh"
-        [ ("stubProperty", stubProperty)
+        [ ("(natToNormalizedSum . toNat) preserves numeric value", toNatAndBackPreservesValue)
         ]
   Control.Monad.unless isSuccess System.Exit.exitFailure
 
-stubProperty :: Hedgehog.Property
-stubProperty = Hedgehog.property $ do
-  number <- Hedgehog.forAll $ Hedgehog.Gen.integral @_ @Int $ Hedgehog.Range.linear 1 100
-  Hedgehog.assert (number > 0)
+genSumDepth :: Int -> Int -> Hedgehog.Gen Tanakh.Sum
+genSumDepth unitCount _depth | unitCount == 0 = return $ Tanakh.Sum []
+genSumDepth _unitCount depth | depth == 0 = return Tanakh.Unit -- might not match unitCount
+genSumDepth unitCount depth | depth == 1 = return $ Tanakh.Sum $ take unitCount $ repeat Tanakh.Unit
+genSumDepth _unitCount _depth = do
+  _listLength <- Hedgehog.Gen.integral @_ @Int $ Hedgehog.Range.linear 1 10
+  undefined -- TODO: pick distribution of units across tree of given depth
+
+genSum :: Hedgehog.Gen Tanakh.Sum
+genSum = do
+  unitCount <- Hedgehog.Gen.integral @_ @Int $ Hedgehog.Range.linear 0 100
+  treeDepth <- Hedgehog.Gen.integral @_ @Int $ Hedgehog.Range.linear 0 5
+  genSumDepth unitCount treeDepth
+
+toNatAndBackPreservesValue :: Hedgehog.Property
+toNatAndBackPreservesValue = Hedgehog.property $ do
+  sumData <- Hedgehog.forAll genSum
+  (Tanakh.sumToNat sumData) === (Tanakh.sumToNat (Tanakh.natToNormalizedSum (Tanakh.sumToNat sumData)))
