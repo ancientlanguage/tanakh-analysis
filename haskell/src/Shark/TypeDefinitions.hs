1ef61ef5cdcf8072175aeac4491c7ed5c48a9367
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2020-04-06 08:11:06 -0700

    Add some comments of the overall approach.

--- haskell/src/Shark/TypeDefinitions.hs
+++ haskell/src/Shark/TypeDefinitions.hs
@@ -80,6 +80,11 @@ data Encoding = Encoding
   }
   deriving (Generic)
 
+-- Sum type
+-- where a value is exactly one of the types in a list
+-- (but not more).
+--
+-- It represents an exclusive "OR" logically.
 data CaseInfo = CaseInfo
   { sizes :: InternalArray Size
   }
@@ -138,3 +143,75 @@ data ValueToArrayError = ValueToArrayError
   , value :: Value
   }
   deriving Generic
+
+-- Natural number as a type "Max Size"
+-- Natural number as a value, should be < max size
+-- defined by our type
+--
+-- Physically on a computer, we start with voltage
+-- above or below a certain threshold to get 0 or 1
+-- 
+-- Then we copy each of those bits to make an array.
+-- "8 bit".
+--
+-- After a Natural number, we want do define everything
+-- else in terms of those two number
+-- (the type size, and the value number).
+--
+-- If we allow ourselves to name two natural numbers,
+-- then we can a primitive of an array.
+-- Where the numbers are:
+-- * element size
+-- * length of the array
+--
+-- By starting number is 10.
+-- Everything you do needs to be less than 10.
+-- Then we can talk about "8 bits".
+-- Array: element size 2 and length 8
+-- and both of those number are less than 10.
+-- 
+-- 32-bits or 64-bits
+-- 31-bits or 16 or 15 bits, historically 7-bits
+--
+-- We can represent the sum type / cases with two concepts
+-- (max size and arrays).
+--
+-- We could use two natural numbers for our CaseInfo.
+--   elemSize is some natural number
+--   length is some natural number
+--
+-- Say we have an 8-bit system
+-- We can define a CaseInfo / SumType as two 8-bit number.
+-- [ bbbb bbbb ] [ bbbb bbbb ]
+--   ^^^^^^^^^ interpret this part as the element size 
+--                 ^^^^^^^^^ interpret as the array length
+--
+-- Example
+-- [ 22 ] [ 1 ]
+-- This can represent Hebrew characters
+-- Each character there is only one option (the letter)
+-- And we have 22 of them.
+--
+-- But Hebrew has final forms, so if we want to 
+-- distinguish each character by final form, then
+-- we need two options for each letter: final or not.
+-- [ 22 ] [ 2 ]
+--          ^^^ value 0 means medial form
+--              value 1 means final form
+--
+-- For a CaseValue, we need to know how big that value
+-- can get, because has to have a max size.
+-- Here the max number is elemSize * arrayLength
+-- (adding up all of the element values).
+-- [ 22 ] [ 1 ] = max size is 22 * 1 = 22
+-- [ 22 ] [ 2 ] = max size of value is 22 * 2 = 44
+--
+-- For a product, the representation is the same,
+-- but the interpretation is different.
+-- [ 22 ] [ 5 ]
+-- That means I have 5 Hebrew letters, which is a short word.
+--
+-- But the product value not elemSize * arrayLength,
+-- instead it's elemSize ^ arrayLength.
+-- [ 22 ] [ 5 ], max size = 22 ^ 5 = 5,153,632
+--
